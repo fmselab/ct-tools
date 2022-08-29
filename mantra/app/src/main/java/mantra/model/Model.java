57943c70d5967679289486b83923d56b79ef7262
unknown <w.essbai@studenti.unibg.it>
2022-08-29 11:17:01 -0500

    modifica tipo restituito da getParameters

--- mantra/app/src/main/java/mantra/model/Model.java
+++ mantra/app/src/main/java/mantra/model/Model.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
+import org.eclipse.emf.common.util.EList;
 import org.pf4j.ExtensionPoint;
 
 import ctwedge.ctWedge.Parameter;
@@ -22,7 +23,7 @@ public interface Model extends ExtensionPoint {
 
 	boolean getUseConstraints();
 
-	Parameter[] getParameters();
+	EList<Parameter> getParameters();
 
 	void translateOutputToString(HashSet<String> tests);
 
