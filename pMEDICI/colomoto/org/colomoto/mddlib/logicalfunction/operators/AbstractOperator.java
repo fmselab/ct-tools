9bd4ca5b3480545c3d00c0946a4e0f6823a52f47
Andrea Bombarda <a.bombarda@studenti.unibg.it>
2023-04-21 18:12:32 +0200

    pMEDICI fix for models with single-value parameters

---
+++ pMEDICI/colomoto/org/colomoto/mddlib/logicalfunction/operators/AbstractOperator.java
@@ -0,0 +1,35 @@
+package org.colomoto.mddlib.logicalfunction.operators;
+
+import org.colomoto.mddlib.MDDManager;
+import org.colomoto.mddlib.logicalfunction.FunctionNode;
+import org.colomoto.mddlib.logicalfunction.FunctionParser;
+
+
+/**
+ * Common methods for internal nodes (operators).
+ *
+ * @author Fabrice Lopez: initial implementation
+ * @author Aurelien Naldi: adaptation
+ */
+public abstract class AbstractOperator implements FunctionNode {
+  protected String returnClassName;
+  protected FunctionParser parser;
+
+  public AbstractOperator() {
+    super();
+  }
+  
+  @Override
+  public String toString() {
+	  return toString(false);
+  }
+  
+  @Override
+  public boolean isLeaf() {
+    return false;
+  }
+
+  public abstract String getSymbol();
+  public abstract int getNbArgs();
+  public abstract FunctionNode[] getArgs();
+}
